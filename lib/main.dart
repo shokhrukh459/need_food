e7abae5eb8d13dc47fb31166fbf7160d9ad2b99b
Shokhrukh <shokhrukh.rakhmatov1234@gmail.com>
2021-12-18 08:17:23 +0500

    +

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:need_food/screens/first_page.dart';
 import 'package:need_food/screens/second_page.dart';
 import 'package:need_food/screens/third_page.dart';
+import 'package:need_food/screens/sign_up.dart';
 
 void main(){
   runApp(MyApp());
@@ -15,10 +16,12 @@ void main(){
        title: "needfood",
        home: Scaffold(
          body: PageView(
+
            children: [
              FirstPage(),
              SecondPage(),
              ThirdPage(),
+             SignUp()
            ],
          ),
        ),
