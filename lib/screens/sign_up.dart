c25582eb21dc73f209e5f1e2ef5f29a255a2d0ac
Shokhrukh <shokhrukh.rakhmatov1234@gmail.com>
2021-12-24 11:28:51 +0500

    h

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -7,47 +8,59 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/bg_image.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 320,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/bg_image.png"),
+              fit:BoxFit.fitHeight,
             ),
-            Text(
-              "Sign in",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Enter your username',
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 320,
+              ),
+              Text(
+                "Sign in",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'required';
+                    }
+                    return null;
+                    if (value.length<8 ){
+                      return 'username should be at least 8 chars';
+                    }
+                    return null;
+                    ?                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                    hintText: 'Enter your username',
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                  ),
                 ),
               ),
-            ),
-            Text("Forgot your password?"),
-          ],
+              Text("Forgot your password?"),
+              SizedBox(height: 300,),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
